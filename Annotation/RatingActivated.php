705283bba17b4bb2975a771bda0f8f8d807433ed
Cymo <tim.gillet@gmail.com>
2018-01-25 18:12:06 +0100

    add: RatingActivated annotation

--- Annotation/RatingActivated.php
+++ Annotation/RatingActivated.php
@@ -10,13 +10,45 @@ use Doctrine\ORM\Mapping\Annotation;
  * @Annotation
  * @Target("CLASS")
  * @Attributes({
+ *   @Attribute("minRating", type = "integer"),
  *   @Attribute("maxRating", type = "integer"),
+ *   @Attribute("ratingStep", type = "mixed"),
  * })
  */
 final class RatingActivated implements Annotation
 {
+    private $minRating;
+    private $maxRating;
+    private $ratingStep;
+
     public function __construct(array $values)
     {
-        $this->maxRating = $values['maxRating'];
+        $this->minRating  = $values['minRating'];
+        $this->maxRating  = $values['maxRating'];
+        $this->ratingStep = $values['ratingStep'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMinRating()
+    {
+        return $this->minRating;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMaxRating()
+    {
+        return $this->maxRating;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRatingStep()
+    {
+        return $this->ratingStep;
     }
 }
