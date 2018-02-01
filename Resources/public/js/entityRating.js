19cff9ffe13c2833091d813967928623ca933bc4
Cymo <tim.gillet@gmail.com>
2018-02-01 19:59:00 +0100

    change: sending a serialized form and possibility to define a success/error-Callback

--- Resources/public/js/entityRating.js
+++ Resources/public/js/entityRating.js
@@ -1,20 +1,26 @@
-/**
- * Created by cymo on 25/01/18.
- */
+var EntityRating = function (options) {
 
-class StarRating {
-    constructor(options) {
-        this.options = options;
-        this.options.starItem.on('click', this.starClicked);
-        console.log(options);
-    }
-
-    starClicked(e) {
-        console.log(e.target);
+    var self = this;
+    var sendForm = function () {
         $.post({
-            url : Routing.generate('cymo_entity_rating_rate', {type : 'itinerary', id : 1, rate : e.target.value})
-        }).then((response) => {
-            console.log(response);
+            url  : Routing.generate('cymo_entity_rating_rate', {
+                id   : self.options.form.find('input.entity-id').val(),
+                type : self.options.form.find('input.entity-type').val()
+            }),
+            data : self.options.form.serialize()
+        }).then(function (response) {
+            if (self.options.successCallback !== undefined) {
+                self.options.successCallback(response);
+            }
+        }).fail(function (response) {
+            if (self.options.errorCallback !== undefined) {
+                self.options.errorCallback(response);
+            }
         });
-    }
-}
+    };
+
+    (function __construct() {
+        self.options = options;
+        self.options.form.find(self.options.radioButtonClass).change(sendForm);
+    })();
+};
